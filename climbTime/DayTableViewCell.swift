9c3007d8abd53a85ccc3112adc30bd77756721e9
lre <lre@2bm.com>
2014-09-19 00:37:54 +0200

    first commit

---
+++ climbTime/DayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DayTableViewCell.swift
+//  climbTime
+//
+//  Created by Lasse Reedtz on 18/09/14.
+//  Copyright (c) 2014 LasseInc. All rights reserved.
+//
+
+import UIKit
+
+class DayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
