f4b7dca8d0d4c28c42df581b283e53f5a4492ae3
lre <lre@2bm.com>
2014-09-19 00:38:12 +0200

    first commit

--- climbTime/DayTableViewCell.swift
+++ climbTime/DayTableViewCell.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import QuartzCore
 
 class DayTableViewCell: UITableViewCell {
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var Day: UILabel!
+    private var signedUp : Bool = false
+    private var meSelf : UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,5 +25,38 @@ class DayTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    private func addImage() {
+        var selfi = UIImage(named: "thumb.jpeg")
+        meSelf = UIImageView(image: selfi)
+        meSelf.frame = CGRect(x: self.frame.size.width - 70, y: 75 / 2 - 25, width: 50.0, height: 50.0)
+        meSelf.layer.cornerRadius = 25
+        meSelf.layer.borderWidth = 1
+        meSelf.layer.masksToBounds = true
+        self.contentView.addSubview(meSelf);
+        
+        
+    }
+    
+    private func removeImage() {
+        meSelf.removeFromSuperview()
+    }
+    
+    func removeMe() {
+        signedUp = false
+        
+        removeImage()
+        }
+    
+    func signUp() {
+        signedUp = true
+        
+        addImage()
+    }
+    
+    func isSignedUp() -> Bool {
+        return signedUp
+    }
+    
 
 }
--- climbTime/DayTableViewController.swift
+++ climbTime/DayTableViewController.swift
@@ -9,89 +9,140 @@
 import UIKit
 
 class DayTableViewController: UITableViewController {
-
+    
+    let cellId = "daycell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.tableView.rowHeight = 75
+        self.tableView.decelerationRate = UIScrollViewDecelerationRateFast
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
-        // #warning Potentially incomplete method implementation.
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // Return the number of sections.
-        return 0
+        return 1
     }
-
-    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
-        return 0
+        return 200
     }
-
-    /*
-    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath) as DayTableViewCell
+        
+        var nextDate = NSTimeInterval(86400 * indexPath.row)
+        var date = NSDate().dateByAddingTimeInterval(nextDate)
+        
+        var dateFormatter = NSDateFormatter()
+        
+        //Day
+        dateFormatter.dateFormat = "EEE"
+        var dayString = dateFormatter.stringFromDate(date)
+        cell.Day.text = String(Array(dayString)[0]).uppercaseString + (dayString as NSString).substringWithRange(NSRange(location: 1,length: 2))
+        
+        //Date
+        dateFormatter.dateFormat = "MMM d."
+        var dateString : String = dateFormatter.stringFromDate(date)
+        cell.Date.text = dateString
+        
+        
+        
         // Configure the cell...
-
         return cell
     }
-    */
-
-    /*
+    
+    
+    
+    
     // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return NO if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
+        
+        var isSignedUp = (tableView.cellForRowAtIndexPath(indexPath) as DayTableViewCell).isSignedUp()
+        
+        var action : UITableViewRowAction
+        
+        if(!isSignedUp){
+            action = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Add Me",
+                handler:{
+                    (action: UITableViewRowAction!, indexPath: NSIndexPath!)
+                    in (tableView.cellForRowAtIndexPath(indexPath) as DayTableViewCell).signUp()
+                    self.tableView.editing = false
+                    return
+            })
+            
+            action.backgroundColor = UIColor.greenColor();
+            
+        }
+        else {
+            action = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Remove Me",
+                handler:{
+                    (action: UITableViewRowAction!, indexPath: NSIndexPath!)
+                    in (tableView.cellForRowAtIndexPath(indexPath) as DayTableViewCell).removeMe()
+                    self.tableView.editing = false
+                    return
+            })
+            
+            action.backgroundColor = UIColor.redColor()
+            
+        }
+        return [action]
+    }
+    
     // Override to support editing the table view.
-    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
+    
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    // Return NO if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
